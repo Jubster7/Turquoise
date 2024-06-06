174b4d8d0c03cc8b715427edec5346bfe589fb93
Jude <jude@Judes-Air.oxley.nsw.edu.au>
2024-06-06 10:40:10 +1000

    Finished re-adding files

--- Turquoise Compiler/obj/Debug/net8.0/Turquoise Compiler.AssemblyInfo.cs
+++ Turquoise Compiler/obj/Debug/net8.0/Turquoise Compiler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Turquoise Compiler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfc6eee1d6ae84f2fe141451400ea3c5193909e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Turquoise Compiler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Turquoise Compiler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
