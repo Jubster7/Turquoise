cbcbc10bfe7a3a2ac3a5b26544ca988133909d37
Jude <jude@Judes-Air.oxley.nsw.edu.au>
2024-06-06 14:47:02 +1000

    updated readme and added executable and object file

--- Turquoise Compiler/Program.cs
+++ Turquoise Compiler/Program.cs
@@ -3,6 +3,8 @@
 class Program {
 	static string in_file_path = @"turquoise.tq";
 	const string out_file_path = @"out.asm";
+	const string assembler_command = @"nasm -f macho64 -o out.o out.asm ";
+	const string linker_command = @"ld -e _start out.o -o out -macosx_version_min 11.0 -L /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib -lSystem";
 
     static void Main(string[] args) {
 		if (args.Length > 0) {
